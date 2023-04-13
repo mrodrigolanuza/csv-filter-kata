706a6e4d1a07a61cea99d86c6c0ab97de7a14a0a
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-13 20:28:58 +0200

    Refactor: Dar semántica a la comprobación del cálculo de impuestos correctamente

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -10,16 +10,25 @@ export class CSVFilter{
         const result = [];
         result.push(this.lines[0]);
         const fields = this.lines[1].split(',');
+        const grossAmount = fields[2];
+        const netAmout = fields[3]
         const ivaField = fields[4];
         const igicField = fields[5];
         const regexDecimal = /[0-9]+/;
         const taxFieldAreMutuallyExclusive = (ivaField.match(regexDecimal) && !igicField)|| (!ivaField && igicField.match(regexDecimal));
         
         
-        if(taxFieldAreMutuallyExclusive && parseFloat(fields[3]) === parseFloat(fields[2]) - (parseFloat(fields[2]) * parseFloat(ivaField)) / 100){
+        if(taxFieldAreMutuallyExclusive && this.checkIfNetAmountIsCorrect(netAmout, grossAmount, ivaField)){
             result.push(this.lines[1]);
         }
 
         return result;
+    }
+
+    private checkIfNetAmountIsCorrect(netAmountField:string, grossAmountField:string, taxField: string){
+        const parsedNetAmount = parseFloat(netAmountField);
+        const parsedGrossAmount = parseFloat(grossAmountField);
+        const parsedTax = parseFloat(taxField);
+        return parsedNetAmount === parsedGrossAmount - (parsedGrossAmount * parsedTax) / 100;
     }
 }
