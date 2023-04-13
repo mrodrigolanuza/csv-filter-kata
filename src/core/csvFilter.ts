05d672128a712c24010f1936597c94bfc4beeb48
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-13 18:43:20 +0200

    Green: Eliminar líneas con impuestos IVA e IGIC

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -7,6 +7,13 @@ export class CSVFilter{
     }
 
     get filteredLines(){
-        return this.lines;
+        const result = [];
+        result.push(this.lines[0]);
+        const fields = this.lines[1].split(',');
+        if(!fields[4] || !fields[5]){
+            result.push(this.lines[1]);
+        }
+
+        return result;
     }
 }
