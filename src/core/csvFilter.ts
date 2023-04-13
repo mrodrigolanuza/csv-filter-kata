0325435e1080b44dfd05ce6782926da35382f9b7
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-13 18:51:26 +0200

    Green: Eliminar líneas sin IVA ni IGIC

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -10,7 +10,7 @@ export class CSVFilter{
         const result = [];
         result.push(this.lines[0]);
         const fields = this.lines[1].split(',');
-        if(!fields[4] || !fields[5]){
+        if((fields[4] && !fields[5])|| (!fields[4] && fields[5])){
             result.push(this.lines[1]);
         }
 
