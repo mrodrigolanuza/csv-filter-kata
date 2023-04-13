da378f6a721d25aecf52fcdfdc55a2c0af4c8a62
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-13 20:21:06 +0200

    Green: Eliminar líneas con neto mal calculado

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -14,7 +14,9 @@ export class CSVFilter{
         const igicField = fields[5];
         const regexDecimal = /[0-9]+/;
         const taxFieldAreMutuallyExclusive = (ivaField.match(regexDecimal) && !igicField)|| (!ivaField && igicField.match(regexDecimal));
-        if(taxFieldAreMutuallyExclusive){
+        
+        
+        if(taxFieldAreMutuallyExclusive && parseFloat(fields[3]) === parseFloat(fields[2]) - (parseFloat(fields[2]) * parseFloat(ivaField)) / 100){
             result.push(this.lines[1]);
         }
 
