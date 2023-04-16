e65b149fd15dd33f839df1e846b8b22467a0eafe
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2023-04-16 20:25:03 +0200

    Refactor: Sustuir el acumulador mutable result por un estilo más declarativo

--- src/core/csvFilter.ts
+++ src/core/csvFilter.ts
@@ -5,35 +5,30 @@ export class CSVFilter{
     static create(lines:string[]){
         return new CSVFilter(lines);
     }
-
     get filteredLines(){
-        const result = [];
-        result.push(this.lines[0]);
+        const header = this.lines[0];
         const invoices = this.lines.slice(1);
-        invoices.forEach(invoice =>{
-            const fields = invoice.split(',');
-            const grossAmount = fields[2];
-            const netAmout = fields[3]
-            const ivaField = fields[4];
-            const igicField = fields[5];
-            const cifField = fields[7];
-            const nifField = fields[8];
-            const regexDecimal = /[0-9]+/;
-            const taxFieldAreMutuallyExclusive = (ivaField.match(regexDecimal) && !igicField)|| (!ivaField && igicField.match(regexDecimal));
-            const cifNifAreMutiallyExclusive = (!cifField || !nifField);
-            const netAmountIsWellCalculated = 
-                this.checkIfNetAmountIsCorrect(netAmout, grossAmount, ivaField) || 
-                this.checkIfNetAmountIsCorrect(netAmout, grossAmount, igicField);
-            
-            if(taxFieldAreMutuallyExclusive && 
-            cifNifAreMutiallyExclusive && 
-            netAmountIsWellCalculated ){
-                result.push(invoice);
-            }
-        });
-        
-        return result;
+        return [header].concat(
+            invoices.filter(invoice =>{
+                const fields = invoice.split(',');
+                const grossAmount = fields[2];
+                const netAmout = fields[3]
+                const ivaField = fields[4];
+                const igicField = fields[5];
+                const cifField = fields[7];
+                const nifField = fields[8];
+                const regexDecimal = /[0-9]+/;
+                const taxFieldAreMutuallyExclusive = (ivaField.match(regexDecimal) && !igicField)|| (!ivaField && igicField.match(regexDecimal));
+                const cifNifAreMutiallyExclusive = (!cifField || !nifField);
+                const netAmountIsWellCalculated = 
+                    this.checkIfNetAmountIsCorrect(netAmout, grossAmount, ivaField) || 
+                    this.checkIfNetAmountIsCorrect(netAmout, grossAmount, igicField);
+                
+                return taxFieldAreMutuallyExclusive && cifNifAreMutiallyExclusive && netAmountIsWellCalculated;
+            })
+        );
     }
+
 
     private checkIfNetAmountIsCorrect(netAmountField:string, grossAmountField:string, taxField: string){
         const parsedNetAmount = parseFloat(netAmountField);
